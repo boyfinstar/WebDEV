1cb1d97e91eb34a24c24324d9b0c0481f64a0a1a
NikkyXO <alexisdame2017@gmail.com>
2022-09-21 20:47:33 +0100

    using Unsemantic css

---
+++ index.js
@@ -0,0 +1,14 @@
+
+// let key = document.querySelector(".example")
+// key.innerText = " \t <div class=\"grid-container\">\n\
+//   \t\t<div class=\"grid-25\">\n\
+//    \t\t\t  I am 25% wide.\n\
+//   \t\t </div>\n\
+//   \t\t <div class=\"grid-50\">\n\
+//   \t\t  \t  I am 50% wide.\n\
+//   \t\t </div>\n\
+//   \t\t <div class=\"grid-25\">\n\
+//   \t\t  \t  I am 25% wide\n\
+//   \t\t </div>\n\
+// \t </div > "
+
